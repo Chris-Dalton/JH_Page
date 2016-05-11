2bda0954bb6fd92a1bef9b1448ed1041fa83c386
manwithgills <chris.dalton1207@gmail.com>
2016-05-10 22:10:07 -0400

    created thumbnails, created lightbox function, added text for categories as needed.

--- JS/custom.js
+++ JS/custom.js
@@ -1,4 +1,39 @@
 $(document).ready(function() {
+	/* activate the carousel */
+   $("#modal-carousel").carousel({interval:false});
+
+   /* change modal title when slide changes */
+   $("#modal-carousel").on("slid.bs.carousel",       function () {
+        $(".modal-title")
+        .html($(this)
+        .find(".active img")
+        .attr("title"));
+   });
+
+   /* when clicking a thumbnail */
+   $(".row .thumbnail").click(function(){
+    var content = $(".carousel-inner");
+    var title = $(".modal-title");
+  
+    content.empty();  
+    title.empty();
+  
+  	var id = this.id;  
+     var repo = $("#img-repo .item");
+     var repoCopy = repo.filter("#" + id).clone();
+     var active = repoCopy.first();
+  
+    active.addClass("active");
+    title.html(active.find("img").attr("title"));
+  	content.append(repoCopy);
+
+    // show the modal
+  	$("#modal-gallery").modal("show");
+  });
+
+});
+
+// Bootstrap Contact Form Script
     $('#contact_form').bootstrapValidator({
         // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
         feedbackIcons: {
